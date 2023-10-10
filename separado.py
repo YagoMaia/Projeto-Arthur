1a8eea395e17df453e75b1dcf3f864543c10c14e
YagoMaia <yagohenriquev123@gmail.com>
2023-10-10 11:04:51 -0300

    Adicionando timer e transformando para executavel

--- separado.py
+++ separado.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
+import time
+
 
 condicoes = {
         'Pressão Arterial': 0, 
@@ -36,17 +38,23 @@ class Funcs():
         "P1": ["Compressão torácica por 2 minutos", "Desfibrilação", "Amiodarona"],
         "P2":["Compressão torácica por 2 minutos", "Desfibrilação", "Adrenalina"],
         "P3": ["Compressão torácica por 2 minutos", "Desfibrilação", "Amiodarona"]}
-    
-    def ret_texto(self, text):
+
+    def ret_texto(self, text, tempo = 30):
+        if text == "Compressão torácica por 2 minutos":
+            tempo = 120
+        tempo_atual = self.timer.get()
+        m,s = int(tempo_atual[0:2]), int(tempo_atual[3:5])
+        tempo_convertido = (m*60)+s - tempo
+        self.timer_ajuste.set(tempo_convertido)
+        self.change.set("True")
+
         self.mostra.set(f"Ação usada: {text}")
         self.salvamento(text)
-        
+
     def clicked(self,btn_text):
-        print(btn_text)
         self.mostra.set(f"Ação usada: {btn_text}")
 
     def salvamento(self, acao):
-        print(self.tentativas.get())
         valido = True
         if (self.tentativas.get()) == 4:
             messagebox.showinfo("Estado do Paciente", "Todos os procedimentos foram feitos")
@@ -56,19 +64,33 @@ class Funcs():
             lista_procedimentos = self.q1[f"P{self.tentativas.get()}"]
             self.procedimentos_usados.append(acao)
             if len(lista_procedimentos) == len(self.procedimentos_usados):
-                print(lista_procedimentos)
-                print(self.procedimentos_usados)
                 self.procedimentos_usados = []
                 tentativas_totais = self.tentativas.get() + 1
                 self.tentativas.set(tentativas_totais)
                 self.mostra.set(f"Quantidade de Tentativas: {tentativas_totais}")
 
+    def countdown(self, num_sec):
+        m, s = divmod(num_sec,60)
+        min_sec_format = '{:02d}:{:02d}'.format(m, s)
+        self.timer.set(min_sec_format)
+
+        if num_sec > 0:
+            if self.timer_ajuste.get() > 0 and self.change.get() == "True":
+                tempo_total = self.timer_ajuste.get()
+                self.change.set("False")
+            else:
+                tempo_total = num_sec
+            root.after(1000, self.countdown, tempo_total-1)
+        if num_sec == 0:
+            messagebox.showinfo("Tempo esgotado", "Você não conseguiu ajudar o paciente dentro do tempo")
+            self.master.destroy()
+
 class BigLabel(Frame):
     def __init__(self, master):
         self.quadro = StringVar()
         self.quadro.set(f"Quadro 1 - 45% ocorrência")
-        Frame.__init__(self, master, background= "#AFA",)
-        Label(self, textvariable=self.quadro, background="#FFF", font=19, width=30).pack()
+        Frame.__init__(self, master)
+        Label(self, textvariable=self.quadro, font=19, width=30).pack()
 
 class Mylabel(Frame):
         def __init__(self, master, key, value):
@@ -78,27 +100,32 @@ class Mylabel(Frame):
 
 class Buttons(Frame, Funcs):
     def __init__(self, master):
-        Frame.__init__(self, master, background= "#D23")
+        Frame.__init__(self, master)
         self.procedimentos_usados = []
+        self.timer = StringVar()
+        self.timer_ajuste = IntVar()
+        self.change = StringVar()
         self.mostra = StringVar()
         self.tentativas = IntVar()
-        Label(self, textvariable=self.mostra, bg='#8FF', font=(19)).grid(row=0, columnspan=5)
-        Label(self, textvariable=self.tentativas, bg='#24D', font=(19)).grid(row=1, columnspan=5)
+        Label(self, textvariable=self.timer, bg='#8AD', font=(19)).grid(row=0, columnspan=5)
+        Label(self, textvariable=self.mostra, bg='#8FF', font=(19)).grid(row=1, columnspan=5)
+        #Label(self, textvariable=self.tentativas, bg='#24D', font=(19)).grid(row=2, columnspan=5)
+        self.countdown(1200)
         for procedimento in range(0, len(Procedimentos)):
             p = Procedimentos[procedimento]
-            Button(self, text = p, bg='#DDD', width = 30, command=lambda text=p : self.ret_texto(text)).grid(row=procedimento+2, column=1) #Preciso definir o grid
+            Button(self, text = p, bg='#DDD', width = 30, command=lambda text=p : self.ret_texto(text)).grid(row=procedimento+3, column=1) #Preciso definir o grid
         for medicamento in range(0,len(Medicações)):
             m = Medicações[medicamento]
-            Button(self, text = m, bg='#DDD', width = 30, command=lambda text=m : self.ret_texto(text)).grid(row=medicamento+2, column=2) #Preciso definir o grid
+            Button(self, text = m, bg='#DDD', width = 30, command=lambda text=m : self.ret_texto(text)).grid(row=medicamento+3, column=2) #Preciso definir o grid
         for exame in range(0,len(Exames)):
             e = Exames[exame]
-            Button(self, text = e, bg='#DDD', width = 30, command=lambda text=e : self.ret_texto(text)).grid(row=exame+2, column=3) #Preciso definir o grid
-
+            Button(self, text = e, bg='#DDD', width = 30, command=lambda text=e : self.ret_texto(text)).grid(row=exame+3, column=3) #Preciso definir o grid
 
 root = Tk()
 BigLabel(root).pack(expand=True, fill='x', anchor="center")
 for k, v in condicoes.items():
     Mylabel(root, k, v).pack(expand=True, fill="y")
+
 Buttons(root).pack(expand=True)
 
 root.mainloop()
